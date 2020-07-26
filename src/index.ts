5fe00453e4e493a000276340837f4e57d063271b
Changwan Jun <wan2land@gmail.com>
2020-07-26 22:12:04 +0900

    Add classes, interfaces to index

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@
 export * from './interfaces/common'
 export * from './interfaces/connection'
 export * from './interfaces/metadata'
+export * from './interfaces/query-builder'
+export * from './interfaces/query-executor'
 export * from './interfaces/repository'
 
 export { Connection } from './connection/connection'
@@ -14,6 +16,15 @@ export { column } from './indexer/column'
 export { text } from './indexer/text'
 
 export { MetadataStorage } from './metadata/storage'
+
+export { between } from './query-builder/operand/between'
+export { beginsWith } from './query-builder/operand/function'
+
+export { DefaultQueryBuilder } from './query-builder/default-query-builder'
+export { BaseFilterBuilder } from './query-builder/base-filter-builder'
+export { Compiler } from './query-builder/compiler'
+
+export { EntityQueryExecutor } from './query-executor/entity-query-executor'
 
 export { Repository } from './repository/repository'
 export { createOptions } from './repository/create-options'
